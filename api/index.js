077407f30fc5dad7f79554692e2aaec88ad427f6
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-20 22:35:29 -0300

    chore: update shareCode to BXCGWK8WMOI

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=FKBP4L4NFSS&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=BXCGWK8WMOI&afp=tapi100' });
     res.end();
   }
