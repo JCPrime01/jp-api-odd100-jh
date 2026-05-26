2e8f29eefb6ca57efc01e702e05f44899b50b5c7
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-26 12:41:07 -0300

    chore: update shareCode to ZMV2DP4P0WH

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=DLCWVBULSCJ&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=ZMV2DP4P0WH&afp=tapi100' });
     res.end();
   }
