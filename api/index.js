cb7606a92bef9540b9a05917caf56024311026f6
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-27 22:39:35 -0300

    chore: update shareCode to 9YLY6ETXNQ0

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=NAUGU76T3VC&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=9YLY6ETXNQ0&afp=tapi100' });
     res.end();
   }
