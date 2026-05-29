a382c3b8bb7db4e3e3b51d2010dc86d0775ff393
JCPrime01 <vanzokiller@gmail.com>
2026-05-29 12:05:02 -0300

    chore: shareCode NAP0SIAYUWH -> QM37GAS1NGI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=NAP0SIAYUWH&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=QM37GAS1NGI&afp=tapi100' });
     res.end();
   }
