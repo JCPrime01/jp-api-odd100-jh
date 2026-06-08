e54eeed6e56dc82111980892102eabf7c0f56ea7
JCPrime01 <vanzokiller@gmail.com>
2026-06-07 22:17:13 -0300

    chore: shareCode TO8QEC0V9VT -> 6HA18Q50IXA (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=TO8QEC0V9VT&afp=tp' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=6HA18Q50IXA&afp=tp' });
     res.end();
   }
